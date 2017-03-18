2d94bf1d77cbfed0055de2bceab875511b2e3809
mortenjust <morten.just@genstart.dk>
2017-03-17 18:57:43 -0700

    add in the sweet shade

---
+++ Shellpad/Shellpad/TrainerSplitView.swift
@@ -0,0 +1,26 @@
+//
+//  TrainerSplitView.swift
+//  Trainer
+//
+//  Created by Morten Just Petersen on 3/17/17.
+//  Copyright © 2017 Morten Just Petersen. All rights reserved.
+//
+
+import Cocoa
+
+class TrainerSplitView: NSSplitView {
+
+    override var dividerColor: NSColor {
+        return NSColor.black
+    }
+
+    
+    
+    
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
